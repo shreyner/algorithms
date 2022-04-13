c52eb2e2bec517b4668a39fe544a51462100dff6
Aleksandr Shreyner <al.shreyner@gmail.com>
2022-04-13 22:17:55 +0300

    Example use list and q go

--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module algorithms
 
 go 1.18
+
+require github.com/gammazero/deque v0.1.1 // indirect
--- go/main.go
+++ go/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"algorithms/sum"
+	"container/list"
 	"fmt"
 	"time"
+
+	"github.com/gammazero/deque"
 )
+
+type Example struct {
+	name string
+}
 
 func main() {
 	sum := sum.Sum(1, 2)
@@ -13,4 +20,29 @@ func main() {
 	fmt.Println("Hello world")
 	fmt.Printf("Sum 1 + 2 = %v\n", sum)
 	fmt.Println("The time is", timeNow)
+
+	myList := list.New()
+
+	myList.PushBack(1)
+	myList.PushBack(2)
+	myList.PushBack(3)
+	myList.PushBack(4)
+
+	element := myList.Front()
+	for i := 1; i < myList.Len(); i++ {
+		fmt.Printf("Index: %v Value: %v\n", i, element.Value)
+		element = element.Next()
+	}
+
+	q := deque.New()
+
+	q.PushBack(Example{name: "Hello"})
+	q.PushBack(Example{name: "Hello 2"})
+	q.PushBack(Example{name: "Hello 3"})
+
+	for q.Len() > 0 {
+		e := q.PopFront()
+
+		fmt.Printf("value: %v\n", e)
+	}
 }

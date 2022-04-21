a753da221016bbf2fb07f9cbb39e7c0f463031b1
Aleksandr Shreyner <al.shreyner@gmail.com>
2022-04-21 19:31:29 +0300

    Remove comments

--- go/quck_sort/quick_sort.go
+++ go/quck_sort/quick_sort.go
@@ -9,14 +9,6 @@ func QuickSort(arr []int) []int {
 	less := []int{}
 	more := []int{}
 
-	// for i := 1; i < len(arr); i++ {
-	// 	if arr[i] <= pivot {
-	// 		less = append(less, arr[i])
-	// 	} else {
-	// 		more = append(more, arr[i])
-	// 	}
-	// }
-
 	for _, item := range arr[1:] {
 		if item <= pivot {
 			less = append(less, item)
